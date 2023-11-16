97b21ce25e266d8a143bb39542e5417d1f7dac3f
Nick Thacke <ncthacke@gmail.com>
2023-11-16 15:19:17 -0500

    cwd

--- myshell/test.c
+++ myshell/test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
 #include "parser.h"
 
 
@@ -15,8 +17,11 @@ size_t size(char * buffer) {
 void test0() {
     char * buffer = "foo bar > quux | baz bar bis ness";
     size_t length = size(buffer);
+    
+    char cwd [PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
 
-    struct command * command = parse(buffer, length);
+    struct command * command = parse(cwd, buffer, length);
     // free_struct_command(command);
 
     traverse_command(command);
