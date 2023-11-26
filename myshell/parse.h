4a3f3a0ea7a46740b77105e0c3f149ddb518ee3b
Nick Thacke <ncthacke@gmail.com>
2023-11-26 17:08:40 -0500

    added function declarations

--- myshell/parse.h
+++ myshell/parse.h
@@ -1,1 +1,38 @@
+struct command {
+    /**
+     * @brief A series of files which consist of the command.
+     */
+    struct file ** files;
+    /**
+     * @brief The number of files contained within files.
+     */
+    int size;
+};
+
+struct file {
+
+    /**
+     * @brief The name of this file.
+     */
+    char * name;
+    /**
+     * @brief The arguments to the file.
+     */
+    char ** args;
+    /**
+     * @brief The number of arguments to the file.
+     */
+    int size;
+    /**
+     * @brief The redirected output of this file. NULL indicates STDOUT
+     */
+    char * output;
+    /**
+     * @brief The redirected input of this file. NULL indicates STDIN
+     */
+    char * input;
+};
+
 struct command * parse(char * buffer);
+void free_struct_command(struct command * c);
+void traverse_command(struct command * command);
